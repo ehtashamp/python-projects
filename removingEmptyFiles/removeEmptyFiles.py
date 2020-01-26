51f0f484117a528f9eb54b4b748b40d2ba1758cb
Admin <admin@Admins-MacBook-Pro.local>
2020-01-26 23:57:50 +0530

    Python program to remove empty files from a users given directory path.

---
+++ removingEmptyFiles/removeEmptyFiles.py
@@ -0,0 +1,15 @@
+"""
+This program accepts a directory from the user and removes empty files from it.
+It uses OS pacakge and its functions
+"""
+import os
+os.listdir()
+dir_name = (input('Provide a a directory name to remove empty file form: '))
+if os.path.isdir((dir_name)):
+    all_files = os.listdir(dir_name)
+    for file in all_files:
+        filename = os.path.join(dir_name, file)
+        if os.path.isfile(filename):
+            if os.path.getsize(filename) == 0:
+                os.remove( filename)
+                print('removed file %d', filename)
